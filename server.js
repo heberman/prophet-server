7ca32121ffbe6d220b1cecb178dbed06a31590cd
Henry Berman <heberman@calpoly.edu>
2023-04-10 18:29:41 -0700

    Test endpoint 6

--- server.js
+++ server.js
@@ -189,7 +189,7 @@ function makeTrade(user, ticker, numShares, price) {
 
 app.get('/test', async (req, res) => {
     const yesterday = new Date(getDaysAgo(1));
-    yesterday.setHours(8, 0, 0);
+    yesterday.setHours(17, 0, 0);
     console.log(yesterday.toLocaleString());
     return res.send({status: "success"});
 });
